c7c439bb0de6847b1eaa323880d51daf5776aa39
jhechus <jess.osm1412@gmail.com>
2023-01-13 22:17:04 -0600

    se agrega el menu para pantallas pequeñas

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+var hamburguer = document.querySelector(".hamb");
+var navlist = document.querySelector(".nav-list");
+var links = document.querySelector(".nav-list li");
+
+hamburguer.addEventListener("click", function(){
+    this.classList.toggle("click");
+    navlist.classList.toggle("open");
+});
+
 
  var typed = new Typed(".input", {
     strings :[, "(Tu Nombre).", "(Tu Profesion)","(extra)","(otro extra)"],
